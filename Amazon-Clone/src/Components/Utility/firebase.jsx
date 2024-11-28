003569b793766135530343b6293b85bf81ea5eb8
tesfaye88 <123720165+tesfaye88@users.noreply.github.com>
2024-11-28 10:06:52 -0800

    Setting up Firebase

---
+++ Amazon-Clone/src/Components/Utility/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3fcDhsJs8smn8sSQyp81Iu_aBgN0vfAs",
+    authDomain: "clone-9c5bb.firebaseapp.com",
+    projectId: "clone-9c5bb",
+    storageBucket: "clone-9c5bb.firebasestorage.app",
+    messagingSenderId: "582311634121",
+    appId: "1:582311634121:web:31de0abdce064399871918"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = app.firestore()
